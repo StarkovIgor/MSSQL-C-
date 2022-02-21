474aade2e8499415950818bcac955774e2e6536f
StarkovIgor <49684300+StarkovIgor@users.noreply.github.com>
2022-02-21 14:50:07 +0700

    Add files via upload

---
+++ STO_DB/STO_DB/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace STO_DB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.обновитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControlSTO = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridViewOwner = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridViewCar = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridViewWorks = new System.Windows.Forms.DataGridView();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.dataGridViewOrder = new System.Windows.Forms.DataGridView();
+            this.добавитьНарядЗаказToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.tabControlSTO.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOwner)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCar)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewWorks)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrder)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.обновитьToolStripMenuItem,
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьНарядЗаказToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // обновитьToolStripMenuItem
+            // 
+            this.обновитьToolStripMenuItem.Name = "обновитьToolStripMenuItem";
+            this.обновитьToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.обновитьToolStripMenuItem.Text = "Обновить";
+            this.обновитьToolStripMenuItem.Click += new System.EventHandler(this.обновитьToolStripMenuItem_Click);
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // tabControlSTO
+            // 
+            this.tabControlSTO.Controls.Add(this.tabPage1);
+            this.tabControlSTO.Controls.Add(this.tabPage2);
+            this.tabControlSTO.Controls.Add(this.tabPage3);
+            this.tabControlSTO.Controls.Add(this.tabPage4);
+            this.tabControlSTO.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlSTO.Location = new System.Drawing.Point(0, 24);
+            this.tabControlSTO.Name = "tabControlSTO";
+            this.tabControlSTO.SelectedIndex = 0;
+            this.tabControlSTO.Size = new System.Drawing.Size(800, 426);
+            this.tabControlSTO.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridViewOwner);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 400);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Владельцы";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewOwner
+            // 
+            this.dataGridViewOwner.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOwner.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewOwner.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewOwner.Name = "dataGridViewOwner";
+            this.dataGridViewOwner.Size = new System.Drawing.Size(786, 394);
+            this.dataGridViewOwner.TabIndex = 0;
+            this.dataGridViewOwner.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewOwner_CellContentClick);
+            this.dataGridViewOwner.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewOwner_CellValueChanged);
+            this.dataGridViewOwner.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridViewOwner_UserAddedRow);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridViewCar);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 400);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Автомобили";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewCar
+            // 
+            this.dataGridViewCar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewCar.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewCar.Name = "dataGridViewCar";
+            this.dataGridViewCar.Size = new System.Drawing.Size(786, 394);
+            this.dataGridViewCar.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridViewWorks);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(792, 400);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Работы";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewWorks
+            // 
+            this.dataGridViewWorks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewWorks.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewWorks.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewWorks.Name = "dataGridViewWorks";
+            this.dataGridViewWorks.Size = new System.Drawing.Size(786, 394);
+            this.dataGridViewWorks.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.dataGridViewOrder);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(792, 400);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Заказ-наряды";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewOrder
+            // 
+            this.dataGridViewOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOrder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewOrder.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewOrder.Name = "dataGridViewOrder";
+            this.dataGridViewOrder.Size = new System.Drawing.Size(786, 394);
+            this.dataGridViewOrder.TabIndex = 0;
+            // 
+            // добавитьНарядЗаказToolStripMenuItem
+            // 
+            this.добавитьНарядЗаказToolStripMenuItem.Name = "добавитьНарядЗаказToolStripMenuItem";
+            this.добавитьНарядЗаказToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.добавитьНарядЗаказToolStripMenuItem.Text = "Добавить Наряд-Заказ";
+            this.добавитьНарядЗаказToolStripMenuItem.Click += new System.EventHandler(this.добавитьНарядЗаказToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControlSTO);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "СТО у Степаныча";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControlSTO.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOwner)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCar)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewWorks)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrder)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem обновитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControlSTO;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.DataGridView dataGridViewOwner;
+        private System.Windows.Forms.DataGridView dataGridViewCar;
+        private System.Windows.Forms.DataGridView dataGridViewWorks;
+        private System.Windows.Forms.DataGridView dataGridViewOrder;
+        private System.Windows.Forms.ToolStripMenuItem добавитьНарядЗаказToolStripMenuItem;
+    }
+}
+
---
+++ STO_DB/STO_DB/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace STO_DB
+{
+    public partial class Form1 : Form
+    {
+        //объявляем экземпляры для подключения и работой с БД
+        private SqlConnection sqlConnection;
+        private SqlCommandBuilder commandBuilder;
+        private SqlDataAdapter sqlDataAdapter;
+        private DataSet dataSet;
+        bool newRowAdding = false;//состояние добавления строки
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["STO_DB"].ConnectionString);
+            sqlConnection.Open();
+            LoadData();
+        }
+
+        private void LoadData()//метод для первоначальной загрузки данных
+        {
+            try
+            {
+                sqlDataAdapter = new SqlDataAdapter("SELECT *, N'Удалить' AS [Действие]  FROM Owner", sqlConnection);
+                SqlDataAdapter sqlDataAdapterCar = new SqlDataAdapter( "SELECT Model, VIN, Year FROM Car", sqlConnection);
+                SqlDataAdapter sqlDataAdapterWorks = new SqlDataAdapter("SELECT Work, Price FROM Works", sqlConnection);
+                SqlDataAdapter sqlDataAdapterOrder = new SqlDataAdapter("SELECT Owner.Name, Car.Model FROM Owner, Car WHERE Owner.Id = Car.OwnerId", sqlConnection);
+                commandBuilder = new SqlCommandBuilder(sqlDataAdapter);
+
+                commandBuilder.GetDeleteCommand();
+                commandBuilder.GetInsertCommand();
+                commandBuilder.GetUpdateCommand();
+
+                dataSet = new DataSet();
+
+                sqlDataAdapter.Fill(dataSet, "Owner");
+                sqlDataAdapterCar.Fill(dataSet, "Car");
+                sqlDataAdapterWorks.Fill(dataSet, "Works");
+                sqlDataAdapterOrder.Fill(dataSet, "Order");
+                dataGridViewOwner.DataSource = dataSet.Tables["Owner"];
+                for (int i = 0; i < dataGridViewOwner.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                    dataGridViewOwner[6, i] = linkCell;
+                }
+                dataGridViewCar.DataSource = dataSet.Tables["Car"];
+                dataGridViewWorks.DataSource = dataSet.Tables["Works"];
+                dataGridViewOrder.DataSource = dataSet.Tables["Order"];
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message,"Ошибка",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ReloadData()//метод для обновления данных
+        {
+            try
+            {
+
+                dataSet.Tables["Owner"].Clear();
+                sqlDataAdapter.Fill(dataSet, "Owner");
+                dataGridViewOwner.DataSource = dataSet.Tables["Owner"];
+                for (int i = 0; i < dataGridViewOwner.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                    dataGridViewOwner[6, i] = linkCell;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void обновитьToolStripMenuItem_Click(object sender, EventArgs e)//метод подписанный на событие кнопкки обновить
+        {
+            ReloadData();
+        }
+
+        private void dataGridViewOwner_CellContentClick(object sender, DataGridViewCellEventArgs e)//метод подписанный на события клика по ячейке
+        {
+            try
+            {
+                if (e.ColumnIndex == 6)
+                {
+                    string task = dataGridViewOwner.Rows[e.RowIndex].Cells[6].Value.ToString();
+                    if (task == "Удалить")
+                    {
+                        if (MessageBox.Show("Удалить владельца из БД?", "Удаление", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            int rowIndex = e.RowIndex;
+                            dataGridViewOwner.Rows.RemoveAt(rowIndex);
+                            dataSet.Tables["Owner"].Rows[rowIndex].Delete();
+                            sqlDataAdapter.Update(dataSet, "Owner");
+                        }
+                    }
+                    else if (task == "Добавить")
+                    {
+                        int rowIndex = e.RowIndex;
+                        DataRow row = dataSet.Tables["Owner"].NewRow();
+                        row["Name"] = dataGridViewOwner.Rows[rowIndex].Cells["Name"].Value;
+                        row["Surname"] = dataGridViewOwner.Rows[rowIndex].Cells["Surname"].Value;
+                        row["Phone"] = dataGridViewOwner.Rows[rowIndex].Cells["Phone"].Value;
+                        row["Email"] = dataGridViewOwner.Rows[rowIndex].Cells["Email"].Value;
+                        row["Birthday"] = dataGridViewOwner.Rows[rowIndex].Cells["Birthday"].Value;
+
+                        dataSet.Tables["Owner"].Rows.Add(row);
+                        dataSet.Tables["Owner"].Rows.RemoveAt(dataSet.Tables["Owner"].Rows.Count - 1);
+                        dataGridViewOwner.Rows.RemoveAt(rowIndex);
+                        dataGridViewOwner.Rows[rowIndex].Cells[6].Value = "Удалить";
+
+                        sqlDataAdapter.Update(dataSet, "Owner");
+                        newRowAdding = false;
+                    }
+                    else if (task == "Обновить")
+                    {
+                        int rowIndex = e.RowIndex;
+                        dataSet.Tables["Owner"].Rows[rowIndex]["Name"] = dataGridViewOwner.Rows[rowIndex].Cells["Name"].Value;
+                        dataSet.Tables["Owner"].Rows[rowIndex]["Surname"] = dataGridViewOwner.Rows[rowIndex].Cells["Surname"].Value;
+                        dataSet.Tables["Owner"].Rows[rowIndex]["Phone"] = dataGridViewOwner.Rows[rowIndex].Cells["Phone"].Value;
+                        dataSet.Tables["Owner"].Rows[rowIndex]["Email"] = dataGridViewOwner.Rows[rowIndex].Cells["Email"].Value;
+                        dataSet.Tables["Owner"].Rows[rowIndex]["Birthday"] = dataGridViewOwner.Rows[rowIndex].Cells["Birthday"].Value;
+
+                        sqlDataAdapter.Update(dataSet, "Owner");
+                        dataGridViewOwner.Rows[rowIndex].Cells[6].Value = "Удалить";
+                    }
+                    ReloadData();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridViewOwner_UserAddedRow(object sender, DataGridViewRowEventArgs e)//метод обрабатывающий событие добавление строки
+        {
+            try
+            {
+                if (newRowAdding == false)
+                {
+                    newRowAdding = true;//как только внесли данные в ячейку перключаем состояние 
+                    int lastRow = dataGridViewOwner.Rows.Count - 2;//находим индекс последней строки
+                    DataGridViewRow row = dataGridViewOwner.Rows[lastRow];
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                    dataGridViewOwner[6, lastRow] = linkCell;
+                    row.Cells["Действие"].Value = "Добавить";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridViewOwner_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (newRowAdding == false)
+                {
+                    int rowIndex = dataGridViewOwner.SelectedCells[0].RowIndex;
+
+                    DataGridViewRow editingRow = dataGridViewOwner.Rows[rowIndex];
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                    dataGridViewOwner[6, rowIndex] = linkCell;
+                    editingRow.Cells["Действие"].Value = "Обновить";
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void добавитьНарядЗаказToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OrderForm form2 = new OrderForm();
+            form2.Show();
+        }
+    }
+}
